54c7bb1a8b0ec7927d0c3c366948b461546d6fb5
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-08-30 12:41:18 -0700

    unnecessary code

--- react-app/src/components/homePage.js
+++ react-app/src/components/homePage.js
@@ -3,28 +3,19 @@ import { NavLink } from "react-router-dom";
 import * as eventActions from '../store/event'
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-// import UsersList from "./UsersList";
-// import '../styles/homePage.css'
-// import likeIcon from '../Images/instagram-like-icon.png';
-// import likedIcon from '../Images/PngItem_5229528.png'
-// import commentIcon from '../Images/instagram-comment-icon.png';
-// import { likeEventThunk } from "../store/event";
 
 const HomePage = () => {
   const { eventId } = useParams()
   const dispatch = useDispatch()
   const events = Object.values(useSelector(state => state.event))
   console.log(events, 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww')
-  // test
+
 
 
   useEffect(() => {
     dispatch(eventActions.getAllEventsThunk())
   }, [dispatch])
 
-  // const handleLikes = async (eventId) => {
-  //   return dispatch(likeEventThunk(eventId))
-  // }
 
   return (
     <>
