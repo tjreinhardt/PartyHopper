6186bf7ce26c84afa39e65a64f42c803df3451dd
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-09-30 13:58:22 -0700

    refactoring for readability

--- react-app/src/components/events/CreateEvent.js
+++ react-app/src/components/events/CreateEvent.js
@@ -5,7 +5,8 @@ import { createEventThunk } from "../../store/event";
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import "react-datepicker/dist/react-datepicker.css";
 import '../../styles/CreateEvent.css'
-import { startDateConversion, dateEquality, getTodaysDate } from "../HelperFunctions/CreateEventHelp";
+// import { startDateConversion, dateEquality, getTodaysDate } from "../HelperFunctions/CreateEventHelp";
+import { isPast, isEqual } from "date-fns";
 
 
 const CreateEventForm = ({ lat, lng }) => {
@@ -20,6 +21,42 @@ const CreateEventForm = ({ lat, lng }) => {
   const [startTime, setStartTime] = useState("")
   const [errors, setErrors] = useState([])
 
+  const startDateConversion = () => {
+    let parts = startDate.split('-')
+    let year = parts[0]
+    let month = parts[1] - 1
+    let day = parts[2]
+    day = Number(day) + 1
+    console.log(year)
+    console.log(month)
+    console.log(day)
+    const result = isPast(new Date(year, month, day))
+    return result
+  }
+
+  const dateEquality = () => {
+    let parts = startDate.split('-')
+    let year = parts[0]
+    let month = parts[1] - 1
+    let day = parts[2]
+    console.log(day, 'day')
+    const result = isEqual(startDate, new Date(year, month, day))
+    return result
+  }
+
+  startDateConversion()
+
+
+  const getTodaysDate = () => {
+    let today = new Date();
+    let todays_day = new Date().getDay() + 18;
+    if (todays_day < 10) todays_day = `0${todays_day}`
+    let todays_month = new Date().getMonth() + 1;
+    if (todays_month < 10) todays_month = `0${todays_month}`
+    let todays_year = today.getFullYear();
+    let todays_date = `${todays_year}-${todays_month}-${todays_day}`
+    return todays_date
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -114,7 +151,7 @@ const CreateEventForm = ({ lat, lng }) => {
             </input>
           </div>
           <div>
-            <input
+            <input className="event-input"
               type={'date'}
               value={startDate}
               onChange={e => setStartDate(e.target.value)}
--- react-app/src/components/events/EventDetail.js
+++ react-app/src/components/events/EventDetail.js
@@ -9,6 +9,7 @@ import GetReviews from "../reviews/ReviewList"
 import CreateReviewForm from "../reviews/CreateReview";
 import { FaStar } from 'react-icons/fa'
 import { rsvpEventThunk } from "../../store/event";
+import { averageReviews, timeConversion, dateConversion, colors } from "../HelperFunctions/EventDetailHelp";
 
 
 
@@ -22,11 +23,6 @@ const EventDetail = () => {
   const [eventIsLoaded, setEventIsLoaded] = useState(false);
   const [editModal, setEditModal] = useState(false);
   const reviewsList = Object.values(reviews)
-
-  const colors = {
-    'yellow': "rgb(219, 142, 0)",
-    'gray': "#a9a9a9"
-  }
 
   const rate = Array(5).fill(0)
 
@@ -49,10 +45,11 @@ const EventDetail = () => {
     return dispatch(rsvpEventThunk(eventId))
   }
 
+
+
   if (!event) {
     return null
   }
-
 
   let userReviewed;
   for (let i = 0; i < reviewsList.length; i++) {
@@ -69,54 +66,7 @@ const EventDetail = () => {
     )
   }
 
-  const averageReviews = (reviewsList) => {
-    let sum = 0;
-    if (reviewsList.length === 0) return
-    if (reviewsList.length === 1) return reviewsList[0].rating - 1
-    if (reviewsList.length === 2) return (((reviewsList[0].rating - 1) + reviewsList[1].rating) / 2)
-    if (reviewsList.length >= 3) {
-      let newArray = []
-      for (let i = 0; i < reviewsList.length; i++) {
-        newArray.push(reviewsList[i].rating)
-        let newValue = reviewsList[i].rating
-        sum += newValue
-      }
-      return sum / reviewsList.length - 1
-    }
-    return ((sum / reviewsList.length)).toFixed(2)
-  }
 
-  const timeConversion = (startTime) => {
-    let parts = startTime.split(":")
-    console.log(parts[0], "parts[0]")
-    if (parts[0] === '00') {
-      return `${(Number(parts[0])) + 12}:${parts[1]} AM`
-    }
-    else if (parts[0] > 12) {
-      return `${(parts[0]) - 12}:${parts[1]} PM`
-    } else return `${startTime} AM`
-  }
-
-  const dateConversion = (startDate) => {
-    let parts = startDate.split("-")
-    let year = parts[0]
-    let month = parts[1]
-    let day = parts[2]
-    let removeZeroes;
-    if (parts[1].startsWith('0')) {
-      removeZeroes = parts[1].split("0")
-      removeZeroes.shift()
-      removeZeroes = removeZeroes[0]
-    } else removeZeroes = month
-
-    let monthsNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    for (let i = 0; i < months.length; i++) {
-      if (`${monthsNumber[i]}` === removeZeroes) {
-        return `${months[i]} ${day}, ${year}`
-      }
-    }
-  }
 
   return (eventIsLoaded && event && <>
     <NavBar />
