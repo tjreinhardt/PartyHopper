76ec7377e54c65861a5990a4abcea05552ab7319
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-10-04 10:48:11 -0700

    rendering event images on homepage now

--- app/seeds/eventphotos.py
+++ app/seeds/eventphotos.py
@@ -4,17 +4,17 @@ def seed_eventphotos():
     photo1 = Eventphoto(
         user_id=1,
         eventId=1,
-        image_url='https://s3-media0.fl.yelpcdn.com/bphoto/sSDWCFmzukeeBLHxz8ghMg/o.jpg'
+        image_url='https://disneycruiselineblog.com/wp-content/uploads/2015/02/DCL-Port-Adventures-Frozen-Norway-Kayaking-in-Geirangerfjord-POV.jpg'
     )
     photo2 = Eventphoto(
         user_id=1,
         eventId=2,
-        image_url='https://s3-media0.fl.yelpcdn.com/bphoto/9BNcEF9QvwnFKp9HIrnv_A/o.jpg'
+        image_url='https://www.jetsetter.com/uploads/sites/7/2018/04/YjRgZpVh-1380x1035.jpeg'
     )
     photo3 = Eventphoto(
         user_id=1,
         eventId=3,
-        image_url='https://s3-media0.fl.yelpcdn.com/bphoto/Cq2f6H8PfDJco2njDOkeGA/o.jpg'
+        image_url='https://images.squarespace-cdn.com/content/v1/52f29591e4b0d3acccdd6286/1512148906579-J57HO2F9X9JETLDPWXJA/matera+adventure+hike+wilderness_DSC8393.jpg'
     )
 
 
